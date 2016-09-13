5e60fd44f47e8c234d647594c046dd751646fd5c
Sofia <emmapohjalainen@gmail.com>
2016-09-13 14:19:47 +0100

    set up gulp for sass and scripts

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,62 @@
 var gulp = require('gulp');
-var path = require('path');
-var swPrecache = require('sw-precache');
-var sourcemaps = require('gulp-sourcemaps');
-var autoprefixer = require('gulp-autoprefixer');
-var browserSync = require('browser-sync').create();
+var sass = require('gulp-sass');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var notify = require('gulp-notify');
+var minifycss = require('gulp-minify-css');
+var concat = require('gulp-concat');
+var plumber = require('gulp-plumber');
+var browserSync = require('browser-sync');
+var neat = require('node-neat');
+var reload = browserSync.reload;
 
-var paths = {
-  src: 'app/'
-};
-
-gulp.task('processCSS', function() {
-    return gulp.src('./css/**/*.css')
-    .pipe(sourcemaps.init())
-    .pipe(autoprefixer())
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest('./css'));
-  });
-
-gulp.task('serve', ['processCSS'], function() {
-
-  browserSync.init({
-      server: './app'
-    });
-
-  gulp.watch('app/css/**/*.scss', ['processCSS']);
+gulp.task('scripts', function() {
+  return gulp.src([
+    /* Add your JS files here, they will be combined in this order */
+    'js/index.js'
+    ])
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest('js'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(uglify())
+    .pipe(gulp.dest('js'));
 });
 
-gulp.task('default', ['serve']);
+/* Sass task */
+gulp.task('sass', function () {
+    gulp.src('css/style.scss')
+    .pipe(plumber())
+    .pipe(sass({
+        includePaths: ['css'].concat(neat)
+    }))
+    .pipe(gulp.dest('css'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(minifycss())
+    .pipe(gulp.dest('css'))
+    /* Reload the browser CSS after every change */
+    .pipe(reload({stream:true}));
+});
+
+/* Reload task */
+gulp.task('bs-reload', function () {
+    browserSync.reload();
+});
+
+/* Prepare Browser-sync for localhost */
+gulp.task('browser-sync', function() {
+    browserSync.init(['css/*.css', 'js/*.js'], {
+        server: {
+            baseDir: './'
+        }
+    });
+});
+
+/* Watch scss, js and html files, doing different things with each. */
+gulp.task('default', ['sass', 'browser-sync'], function () {
+    /* Watch scss, run the sass task on change. */
+    gulp.watch(['scss/*.scss', 'scss/**/*.scss'], ['sass'])
+    /* Watch app.js file, run the scripts task on change. */
+    gulp.watch(['js/app.js'], ['scripts'])
+    /* Watch .html files, run the bs-reload task on change. */
+    gulp.watch(['*.html'], ['bs-reload']);
+});
