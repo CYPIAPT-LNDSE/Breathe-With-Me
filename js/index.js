5106cddc2918f6cc9159fa591f2d55cab423e35c
Sofia <emmapohjalainen@gmail.com>
2016-09-14 10:45:55 +0100

    centering and animation responsiveness

--- js/index.js
+++ js/index.js
@@ -19,6 +19,6 @@ const start = function() {
 
 landingButton.addEventListener('click', start);
 
-const landingCatReveal = TweenMax.to('.landing-cat-round', 2.5, {y:420, ease: Elastic.easeInOut.config(0.5, 0.2), onComplete: function() {
+const landingCatReveal = TweenMax.to('.landing-cat-round', 2.5, {css:{'margin-top':'15%', opacity: 1}, ease: Elastic.easeInOut.config(0.5, 0.2), onComplete: function() {
 TweenMax.to('#landing-button', 0.5, {css:{opacity:1}})
 } })
