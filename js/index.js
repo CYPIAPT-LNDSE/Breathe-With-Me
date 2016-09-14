398548326581dcea74febaae3b5b7bebb03ae2f7
Kara de la Marck <karadelamarck@gmail.com>
2016-09-14 14:06:52 +0100

    Fix: in index.js moves event listener for landing button to bottom of page

--- js/index.js
+++ js/index.js
@@ -1,7 +1,5 @@
 const landingButton = document.getElementById('landing-button');
 
-// landingButton.addEventListener('click', start);
-
 const landingCatReveal = TweenMax.to('.landing-cat-round', 2.5, {css:{'margin-top':'15%', opacity: 1}, ease: Elastic.easeInOut.config(0.5, 0.2), onComplete: function() {
 TweenMax.to('#landing-button', 0.5, {css:{opacity:1}})
 } })
