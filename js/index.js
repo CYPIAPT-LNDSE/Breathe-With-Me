6d8c171026b37d223fb47e89cf07056c041d89d1
Kara de la Marck <karadelamarck@gmail.com>
2016-09-15 19:36:15 +0100

    Feat: color gradient background on well done page

--- js/index.js
+++ js/index.js
@@ -27,9 +27,8 @@ const infoSwitch = function() {
   console.log('button pressed');
   const tl = new TimelineMax();
   tl.add( TweenMax.to('.alt-info-box', 0.2, {css:{ visibility: 'hidden', opacity: 0 }}));
-  // tl.add( TweenMax.to('.name', {text:{value: "OK", delimiter: " "}} ));
   tl.add( TweenMax.to('.breathing-information', 0.2, {css:{visibility: 'visible', opacity: 1 }}));
-  tl.add( TweenMax.to('.name', 0.1, {text:{value: `Hi ${document.cookie}`, delimiter: " "}, ease:Linear.easeNone} ));
+  tl.add( TweenMax.to('.name', 0, {text:{value: `Hi ${document.cookie}`, delimiter: " "}, ease:Linear.easeNone} ));
   tl.add( TweenMax.to('.alt-info-box', 0.2, {css:{ visibility: 'hidden', opacity: 0 }}));
   tl.add( TweenMax.to('.breathing-information', 0.2, {css:{visibility: 'visible', opacity: 1 }}));
 };
@@ -40,7 +39,7 @@ const breathingCat = function() {
   tl.add( TweenMax.to('.mountain2', 0.3, {y: 0}));
   tl.add( TweenMax.to('.mountain1', 0.3, {y: 0}));
   tl.add( TweenMax.to('.breathing-information', 0.5, {css: {visibility: 'hidden', opacity: 0}}));
-  tl.add( TweenMax.fromTo('.alt-intro', 0.2, {backgroundColor: '#5CA1C2'}, {backgroundColor: '#A5E2DA'}));
+  tl.add( TweenMax.fromTo('.alt-intro', 0.2, {css:{'background': '#5CA1C2'}}, {css:{'background': 'linear-gradient(#494A97, #091A3E)'}} ));
   tl.add( TweenMax.fromTo('.breathing', 0.5, {css: {display: 'none'}}, {css: {display: 'block' }}));
   tl.add( TweenMax.to('.mountain3', 0.5, {y: -370, ease: Power2.easeOut}));
   tl.add( TweenMax.fromTo('.cat', 0.75, {css: {opacity: 0}}, {css: {opacity: 1}}));
@@ -62,6 +61,7 @@ const exitToIntro = function() {
 const goToWellDone = function() {
   const tl = new TimelineMax();
   tl.add(TweenMax.to('.breathing', 0.2, {css: { display: 'none'}}));
+  tl.add( TweenMax.fromTo('.alt-intro', 0.1, {backgroundColor: '#A5E2DA'}, {backgroundColor: '#494A97'}));
   tl.add(TweenMax.to('.welldone', 0.2, {css: { display: 'block'}}));
 };
 
