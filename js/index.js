72f602f3570d82866f0eb76212f453ccb13f0708
Sofia <emmapohjalainen@gmail.com>
2016-09-16 15:04:46 +0100

    fix line spacing after pressing start again button

--- js/index.js
+++ js/index.js
@@ -102,7 +102,7 @@ const goToStart = function() {
   tl.add(TweenMax.to('.mountain1', 0.5, {y: -170}));
   tl.add(TweenMax.to('.mountain2', 0.5, {y: -300}));
   tl.add(TweenMax.to('.mountain4', 0.5, {y: -300}));
-  tl.add(TweenMax.to('.breathing-information', 0.2, {css: { display: 'block', opacity: 1, visibility: 'visible'}}));
+  tl.add(TweenMax.to('.breathing-information', 0.2, {css: { display: 'flex', opacity: 1, visibility: 'visible'}}));
 };
 
 const breatheOut = {
