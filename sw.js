7ed9d63a1be0b965288cfe2e69218a523a97fa77
Kara de la Marck <karadelamarck@gmail.com>
2016-09-28 17:21:41 +0100

    Fix: clean up code with better naming in sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,18 @@
 importScripts('/cache-polyfill.js');
 
-// const filesToCache = [
-//   ''
-// ]
+const filesToCache = [
+  '/',
+  '/index.html'
+];
+
+const staticCacheName = 'pages-cache-v1';
+
 self.addEventListener('install', function(event) {
   console.log('Attempting to install service worker and cache static assets');
   event.waitUntil(
-    caches.open('pages-cache-v1')
+    caches.open(staticCacheName)
     .then(function(cache) {
-      return cache.addAll([
-        '/',
-        '/index.html'
-      ]);
+      return cache.addAll(filesToCache);
     })
     .catch(function(error) {
       console.log('Installation failed, ', error);
