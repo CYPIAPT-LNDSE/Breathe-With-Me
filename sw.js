44a65a1b8ac0fa056d48e2c9a95bcc10c7f72142
Kara de la Marck <karadelamarck@gmail.com>
2016-09-28 17:19:03 +0100

    Feat: using service worker to cache static assets

--- sw.js
+++ sw.js
@@ -1,1 +1,20 @@
 importScripts('/cache-polyfill.js');
+
+// const filesToCache = [
+//   ''
+// ]
+self.addEventListener('install', function(event) {
+  console.log('Attempting to install service worker and cache static assets');
+  event.waitUntil(
+    caches.open('pages-cache-v1')
+    .then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html'
+      ]);
+    })
+    .catch(function(error) {
+      console.log('Installation failed, ', error);
+    })
+  );
+});
