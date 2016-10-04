ca31a34afc9c427539e2b03e1520834eaa8f3f94
bradreeder <bradjohnreeder@gmail.com>
2016-10-04 20:43:31 +0100

    updated the service worker
    updated the service worker so that it: deletes items from the cache if its outdated, and updates the cache if a file not in the cache is requested.

--- service-worker.js
+++ service-worker.js
@@ -31,12 +31,42 @@ self.addEventListener('install', function(event) {
   );
 });
 
+self.addEventListener('activate', function(event) {
+  console.log('ServiceWorker activating...');
+
+  const cacheWhiteList = [staticCacheName];
+
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheWhiteList.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
 self.addEventListener('fetch', function(event) {
   console.log(event.request.url);
   event.respondWith(
     caches.match(event.request)
     .then(function(response) {
-      return response || fetch(event.request);
+      if (response) {
+        return response;
+      } else { return fetch(event.request); }
+    })
+    .then(function(response) {
+      return caches.open(staticCacheName).then(function(cache) {
+        cache.put(event.request.url, response.clone());
+        return response;
+      });
+    })
+    .catch(function(error) {
+      console.log('Resource could not be found: ', error);
+      //respond with custom offline page here...
     })
   );
 });
