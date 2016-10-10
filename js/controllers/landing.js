e8ec445e9a26bd0e47d47ddd223a1883374c4fb3
bradreeder <bradjohnreeder@gmail.com>
2016-10-10 20:54:48 +0100

    fixed bug with audio volume on landing

--- js/controllers/landing.js
+++ js/controllers/landing.js
@@ -1,24 +1,26 @@
 import { outOfLanding, landingAnimation } from '../animations';
 
+const audioControl = document.getElementById('audio-controls');
+const audio = document.getElementById('audio');
+
 const toggleAudio = (e) => {
-  if (e.target.textContent === "volume_off") {
-    e.target.textContent = "volume_up";
+  if (e.target.textContent === 'volume_off') {
+    e.target.textContent = 'volume_up';
     audio.play();
   } else {
-    e.target.textContent = "volume_off";
+    e.target.textContent = 'volume_off';
     audio.pause();
   }
 };
 
+audioControl.addEventListener('click', toggleAudio);
+audio.volume = 0.3;
+
 const landingCtrl = () => {
   const landingButton = document.getElementById('landing-button');
-  const audioControl = document.getElementById('audio-controls');
-  const audio = document.getElementById('audio');
 
   landingButton.addEventListener('click', outOfLanding);
-  audioControl.addEventListener('click', toggleAudio);
 
-  audio.volume = 0.3;
   landingAnimation();
 };
 
