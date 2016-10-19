101886ff7bd97192874929852880ca89ae2ca782
bradreeder <bradjohnreeder@gmail.com>
2016-10-19 15:22:44 +0100

    added validation to user input

--- src/controllers/landing.js
+++ src/controllers/landing.js
@@ -1,11 +1,27 @@
-import { askNameToBreathingInfo, nameToInfoSwitch } from '../animations';
+import blacklist from '../lib/blacklist';
 
 const landingCtrl = () => {
   const nameSubmitButton = document.getElementById('name-question-button');
+  const inputValidation = document.getElementById('input-validation');
 
-  nameSubmitButton.addEventListener('click', () => {
+  nameSubmitButton.addEventListener('click', function (event) {
+    event.preventDefault();
     const name = document.querySelector('#input-username').value;
-    localStorage.setItem('username', name);
+    const isEmptyName = !name.trim().length;
+    const isBlacklistedWord = name.trim()
+      .split(/\s/)
+      .some(word => !!blacklist[word]);
+    if (isEmptyName) {
+      inputValidation.textContent = 'Please enter a name!';
+      inputValidation.style.visibility = 'visible';
+    } else if (isBlacklistedWord) {
+      inputValidation.textContent = 'Please enter a different name!';
+      inputValidation.style.visibility = 'visible';
+    } else {
+      inputValidation.style.visibility = 'hidden';
+      localStorage.setItem('username', name);
+      location.hash = this.attributes.getNamedItem('href').value;
+    }
   });
 };
 
