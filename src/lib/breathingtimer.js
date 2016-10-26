45e04b271ad34ad2233fc492bcec8345ea52b38a
bradreeder <bradjohnreeder@gmail.com>
2016-10-26 22:01:03 +0100

    updated notifications on breathing page
    updated the breathing notifications to incorporate instructions on how to breathe. related #278

--- src/lib/breathingtimer.js
+++ src/lib/breathingtimer.js
@@ -8,10 +8,58 @@ let timer4;
 let timer5;
 let timer6;
 let timer7;
+let timer8;
+let timer9;
+let timer10;
+let timer11;
+let timer12;
+let timer13;
 
-const intervals = [7000, 15000, 26000, 37000, 60000, 120000, 300000];
-const messages = ['#first-msg', '#second-msg', '#third-msg', '#fourth-msg', '#fifth-msg', '#after-two-min', '#after-five-min'];
-const timers = [timer1, timer2, timer3, timer4, timer5, timer6, timer7];
+const intervals = [
+  7000,
+  15000,
+  23000,
+  31000,
+  39000,
+  47000,
+  55000,
+  63000,
+  71000,
+  82000,
+  103000,
+  120000,
+  300000,
+];
+const messages = [
+  '#first-msg',
+  '#second-msg',
+  '#third-msg',
+  '#fourth-msg',
+  '#fifth-msg',
+  '#sixth-msg',
+  '#seventh-msg',
+  '#eigth-msg',
+  '#ninth-msg',
+  '#tenth-msg',
+  '#eleventh-msg',
+  '#after-two-min',
+  '#after-five-min',
+];
+const timers = [
+  timer1,
+  timer2,
+  timer3,
+  timer4,
+  timer5,
+  timer6,
+  timer7,
+  timer8,
+  timer9,
+  timer10,
+  timer11,
+  timer12,
+  timer13,
+];
 
 const timeHasPassedNotification = (time, targetEl, timer) => {
   timers[timer] = setTimeout(() => {
